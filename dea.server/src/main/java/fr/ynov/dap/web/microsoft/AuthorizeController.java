569a6f85b57cce5e96821953c4d88b3f0508d667
Anthony <antodu73@hotmail.fr>
2018-11-21 09:38:51 +0100

    MAJ du projet

--- dea.server/src/main/java/fr/ynov/dap/web/microsoft/AuthorizeController.java
+++ dea.server/src/main/java/fr/ynov/dap/web/microsoft/AuthorizeController.java
@@ -85,8 +85,3 @@ public class AuthorizeController
 }
 
 
-
-
-
-
-Affichage des résultats
--- dea.server/src/main/java/fr/ynov/dap/web/microsoft/auth/AuthHelper.java
+++ dea.server/src/main/java/fr/ynov/dap/web/microsoft/auth/AuthHelper.java
@@ -5,6 +5,7 @@ package fr.ynov.dap.web.microsoft.auth;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Calendar;
 import java.util.Properties;
 import java.util.UUID;
 import org.springframework.web.util.UriComponentsBuilder;
