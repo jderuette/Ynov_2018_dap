e854dc9ccdaceccc50b3a43fa7aed3a734299a62
Adrien Jalabert <35221770+ajalabert@users.noreply.github.com>
2018-11-27 10:32:56 +0100

    improve admin interface

--- jaa.server/src/main/java/fr/ynov/dap/services/AdminService.java
+++ jaa.server/src/main/java/fr/ynov/dap/services/AdminService.java
@@ -16,12 +16,14 @@ import com.google.api.client.auth.oauth2.StoredCredential;
 import fr.ynov.dap.data.AppUser;
 import fr.ynov.dap.data.AppUserRepository;
 import fr.ynov.dap.data.MicrosoftAccount;
+import fr.ynov.dap.exceptions.ServiceException;
 import fr.ynov.dap.models.AccountData;
 import fr.ynov.dap.models.AccountData.Type;
 import fr.ynov.dap.services.google.GoogleAuthorizationFlowService;
 
 /**
  * Admin service, used by the AdminController.
+ * TODO Extract methods.
  */
 @Service
 public class AdminService {
@@ -42,8 +44,14 @@ public class AdminService {
     @Autowired
     private GoogleAuthorizationFlowService googleDataStoreService;
 
-
-    public List<AccountData> getAccountsInformations() throws GeneralSecurityException, IOException{
+    /**
+     * Display accounts informations.
+     * @return Several Account informations inside the AccountData model.
+     * @throws ServiceException exception
+     * @throws GeneralSecurityException exception.
+     * @throws IOException exception.
+     */
+    public List<AccountData> getAccountsInformations() throws ServiceException {
         log.info("Accounts informations requested by the admin.");
         List<AccountData> accounts = new ArrayList<AccountData>();
 
@@ -52,10 +60,16 @@ public class AdminService {
         for (AppUser appUser : appUsers) {
             // Google
             List<String> googleAccountNames = appUser.getGoogleAccountNames();
-            Map<String, StoredCredential> googleMap = googleDataStoreService.getStoreCredentialMap();
+            Map<String, StoredCredential> googleMap;
+            try {
+                googleMap = googleDataStoreService.getStoreCredentialMap();
+            } catch (GeneralSecurityException | IOException e) {
+                log.error("Fail to get StoreData", e);
+                throw new ServiceException("An error occured while retrieve Google StoreDara", e);
+            }
             for (String name : googleAccountNames) {
                 StoredCredential googleAccount = googleMap.get(name);
-                if(googleAccount != null) {
+                if (googleAccount != null) {
                     AccountData account = new AccountData();
                     account.setUserKey(appUser.getUserKey());
                     account.setAccountName(name);
@@ -73,16 +87,20 @@ public class AdminService {
                 AccountData account = new AccountData();
                 account.setUserKey(appUser.getUserKey());
                 account.setAccountName(microsoftAccount.getAccountName());
-                account.setAccessToken(microsoftAccount.getTokenResponse().getAccessToken());
-                account.setRefreshToken(microsoftAccount.getTokenResponse().getRefreshToken());
-                account.setExpirationTimeMilliseconds(microsoftAccount.getIdToken().getExpirationTime() * 1000);
+                try {
+                    account.setAccessToken(microsoftAccount.getTokenResponse().getAccessToken());
+                    account.setRefreshToken(microsoftAccount.getTokenResponse().getRefreshToken());
+                    account.setExpirationTimeMilliseconds(microsoftAccount.getIdToken().getExpirationTime() * 1000);
+                    account.setTenantId(microsoftAccount.getIdToken().getTenantId());
+                } catch (IOException e) {
+                    log.error("Error occured during the microsoftAccount reading", e);
+                    throw new ServiceException("Faild to get some properties of Microsoft Account", e);
+                }
                 account.setAccountType(Type.Microsoft);
-                account.setTenantId(microsoftAccount.getIdToken().getTenantId());
                 accounts.add(account);
             }
         }
 
         return accounts;
     }
-
 }
--- jaa.server/src/main/java/fr/ynov/dap/web/AdminController.java
+++ jaa.server/src/main/java/fr/ynov/dap/web/AdminController.java
@@ -1,34 +1,46 @@
 package fr.ynov.dap.web;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import fr.ynov.dap.exceptions.ServiceException;
 import fr.ynov.dap.models.AccountData;
 import fr.ynov.dap.services.AdminService;
+import fr.ynov.dap.web.api.DapController;
 
 /**
  * Admin page.
  */
 @Controller
-public class AdminController {
+public class AdminController extends DapController {
     /**
      * Instantiate the Admin service using injection of dependency.
      */
     @Autowired
     private AdminService adminService;
 
+    /**
+     * Display Account infos.
+     * @param model AccountData model.
+     * @param request request
+     * @return the name of the page that will be displayed.
+     */
     @RequestMapping("/admin")
-    public String microsoftIndex(final ModelMap model, final HttpServletRequest request) throws GeneralSecurityException, IOException {
+    public String microsoftIndex(final ModelMap model, final HttpServletRequest request) {
 
         model.addAttribute("fragment", "fragments/adminFragment");
 
-        List<AccountData> accounts = adminService.getAccountsInformations();
-        model.addAttribute("account", accounts);
+        List<AccountData> accounts;
+        try {
+            accounts = adminService.getAccountsInformations();
+            model.addAttribute("account", accounts);
+        } catch (ServiceException e) {
+            model.addAttribute("error", e);
+        }
         return "base";
     }
 
